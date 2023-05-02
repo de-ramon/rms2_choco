b0b47e795a3d1b09970c9ab133ff505302daad2e
de-ramon <me@rms2.de>
2023-05-02 11:38:52 +0200

    Improve code

--- rms2_choco_dl.ps1
+++ rms2_choco_dl.ps1
@@ -1,8 +1,8 @@
 $InstallPath = "c:\Admin"
-md -Force $InstallPath\Source
+mkdir -Force $InstallPath\Source
 Invoke-WebRequest 'https://codeload.github.com/de-ramon/rms2_choco/zip/refs/heads/master' -OutFile $InstallPath\Source\rms2_choco.zip
 Expand-Archive $InstallPath\Source\rms2_choco.zip $InstallPath
-md -Force $InstallPath\rms2_choco
+mkdir -Force $InstallPath\rms2_choco
 Move-Item -Force -Path $InstallPath\rms2_choco-master\* -Destination $InstallPath\rms2_choco
 Remove-Item $InstallPath\rms2_choco-master -Force
 
