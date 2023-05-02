077e0c6c7845bd8bb1ba7c2f77c98996bd9be3a2
de-ramon <me@rms2.de>
2023-05-02 11:35:46 +0200

    Improve powershell code

--- rms2_choco_dl.ps1
+++ rms2_choco_dl.ps1
@@ -1,7 +1,8 @@
-mkdir c:\Admin\Source | Set-Location
-Invoke-WebRequest 'https://codeload.github.com/de-ramon/rms2_choco/zip/refs/heads/master' -OutFile .\rms2_choco.zip
-Expand-Archive .\rms2_choco.zip c:\Admin\
-mkdir c:\Admin\rms2_choco
-Move-Item -Force -Path C:\Admin\rms2_choco-master\* -Destination C:\Admin\rms2_choco
-Remove-Item C:\Admin\rms2_choco-master -Force
+$InstallPath = "c:\Admin"
+md -Force $InstallPath\Source
+Invoke-WebRequest 'https://codeload.github.com/de-ramon/rms2_choco/zip/refs/heads/master' -OutFile $InstallPath\Source\rms2_choco.zip
+Expand-Archive $InstallPath\Source\rms2_choco.zip $InstallPath
+md -Force $InstallPath\rms2_choco
+Move-Item -Force -Path $InstallPath\rms2_choco-master\* -Destination $InstallPath\rms2_choco
+Remove-Item $InstallPath\rms2_choco-master -Force
 
