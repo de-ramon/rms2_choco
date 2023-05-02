5f1b509fd8db1a7ce1cad7ea4acd38a78acf628f
de-ramon <me@rms2.de>
2023-05-02 11:18:20 +0200

    Get rid of old DOS-commands

--- rms2_choco_dl.ps1
+++ rms2_choco_dl.ps1
@@ -1,4 +1,4 @@
-mkdir c:\Admin\Source | cd
+mkdir c:\Admin\Source | Set-Location
 Invoke-WebRequest 'https://codeload.github.com/de-ramon/rms2_choco/zip/refs/heads/master' -OutFile .\rms2_choco.zip
 Expand-Archive .\rms2_choco.zip c:\Admin\
 mkdir c:\Admin\rms2_choco
