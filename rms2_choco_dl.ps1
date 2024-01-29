bacaf131517025f5c54ed296d682cda964075fe6
de-ramon <me@rms2.de>
2024-01-29 06:41:08 +0100

    rms2_choco_dl.ps1: Creation of desktop icons

--- rms2_choco_dl.ps1
+++ rms2_choco_dl.ps1
@@ -1,8 +1,11 @@
+# Set Destination
 $InstallPath = "c:\Admin"
 mkdir -Force $InstallPath\Source
+# Download package
 Invoke-WebRequest 'https://codeload.github.com/de-ramon/rms2_choco/zip/refs/heads/master' -OutFile $InstallPath\Source\rms2_choco.zip
 Expand-Archive $InstallPath\Source\rms2_choco.zip $InstallPath
 mkdir -Force $InstallPath\rms2_choco
 Move-Item -Force -Path $InstallPath\rms2_choco-master\* -Destination $InstallPath\rms2_choco
-Remove-Item $InstallPath\rms2_choco-master -Force
-
+Remove-Item -Force $InstallPath\rms2_choco-master
+# Copy LNK-Files to the Desktop of the current users
+Copy-Item $InstallPath\rms2_choco\*.lnk ([Environment]::GetFolderPath("Desktop"))
